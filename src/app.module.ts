37c62f05ffaa98bf12811114361353ca33349a22
Serchandro62 <sergio.angel.lopez.arteaga@gmail.com>
2025-09-15 11:47:41 -0600

    importamos ConfigModule. seteamos autoLoadEntities = true.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config'; //Carga variables de entorno desde un archivo .env o directamente del entorno del sistema.
 
 @Module({
   imports: [TypeOrmModule.forRoot({ //se encarga de configurar la conexión global a la base de datos de TypeORM para toda tu aplicación.
     type: 'postgres',
     host: process.env.host,
-    port: +process.env.port!, //Con "!" le aseguramos que no será nulo 
+    port: Number(process.env.port), //Con "!" le aseguramos que no será nulo 
     username: 'postgres',
-    password: "TheBestPassword",
+    password: process.env.pass,
     database: process.env.name,
     entities: [],
+    autoLoadEntities: true,
     synchronize: true,
   }),
     EmployeesModule,
