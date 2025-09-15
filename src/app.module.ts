1dd4eb529d859961e6a58c1de1d7823346b60b05
Serchandro62 <sergio.angel.lopez.arteaga@gmail.com>
2025-09-14 20:12:42 -0600

    1.- Instalamos TypeORM y postgres. //2.- añadimos el TypeOrmModule y sus configuraciones en app.module. //3.- Actualizamos las variables de entorno. //4.- descargamos TablePlus (o algún otro manejador de bases de datos).

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [TypeOrmModule.forRoot({ //se encarga de configurar la conexión global a la base de datos de TypeORM para toda tu aplicación.
+    type: 'postgres',
+    host: process.env.host,
+    port: +process.env.port!, //Con "!" le aseguramos que no será nulo 
+    username: 'postgres',
+    password: "TheBestPassword",
+    database: process.env.name,
+    entities: [],
+    synchronize: true,
+  }),
+    EmployeesModule,
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
