3b6c50471073b3412bbed2151cf5e87d00523f75
Serchandro62 <sergio.angel.lopez.arteaga@gmail.com>
2025-09-23 18:41:09 -0600

    JWT creacion 1.- Instalamos jsonwebtoken (jwt) y su tipado 2.- al loggearse, el usuario recibe un JWT en string. Hay muchas notas

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,45 +5,59 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { QueryFailedError, Repository } from 'typeorm';
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 
 
 @Injectable()
 export class UserService {
 
-  constructor(@InjectRepository(User) private userRepository: Repository<User>){}
-  /**
-   * El segundo parámetro de hash es costo: Es qué tan "profundo" se hashea. Los hackers buscan una palabra e intentan hashearla. Si el resultado es igual
-   * entonces saben la palabra original+salt. Mientras más profundo se hashea, más tiempo le toma al hacker generar cada intento. 
-   */
-  //El "salt" es una cadena aleatoria que se genera y se añade a la contraseña antes de hashearla. Sin esto, 2 contraseñas iguales tendrían el mismo hasheo si tienen la misma "profundidad".
-  //Para producir un mismo hash se necesita: <contraseña + salt + coste> correctos. 
-  async registerUser(createUserDto: CreateUserDto) {
-  try {
-    createUserDto.userPassword = await bcrypt.hash(createUserDto.userPassword, 10);
-    return await this.userRepository.save(createUserDto);
-  } catch (error) {
-    if (error instanceof QueryFailedError) {
-      if ((error as any).code === '23505') { // 23505 = unique_violation en Postgres
-        throw new ConflictException('User with this email already exists');
-      }
-    }
-    // otros errores
-    throw new InternalServerErrorException('User creation failed');
-  }
-}
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
 
-  async loginUser(createUserDto: CreateUserDto){
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  async registerUser(createUserDto: CreateUserDto) {
+    try {
+      createUserDto.userPassword = await bcrypt.hash(createUserDto.userPassword, 10);
+      return await this.userRepository.save(createUserDto);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        if ((error as any).code === '23505') { // 23505 = unique_violation en Postgres
+          throw new ConflictException('User with this email already exists');
+        }
+      }
+      throw new InternalServerErrorException('User creation failed');
+    }
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  async loginUser(createUserDto: CreateUserDto) {
     const user = await this.userRepository.findOneBy({
       userEmail: createUserDto.userEmail
     })
-    if(!user) throw new NotFoundException
+    if (!user) throw new NotFoundException
+    const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword); 
+    if (!match) throw new UnauthorizedException('No estás autorizado');
+    return jwt.sign(JSON.stringify(user),"SECRET KEY"); //Si todo salió bien le damos su token
+    //el primer parámetro tiene que ser un JSON, no un objeto. Por eso lo convertimos. 
+
     /**
-     * La "salt" y el coste vienen explícitamente en el resultado de un hasheo, de forma que se puede comparar una contraseña ingresada con la original.
-     * Aún si el atacante logra ver el hasheo (que tiene la salt y coste), tiene que decifrar la cadena original, lo que le toma mucho tiempo (más si hay más coste)
+    * JWT No es por seguridad, solo es para validar que ningún contenido se ha modificado. Si algo del cuerpo se modifica,
+    * entonces la signature ya no es correcta. 
+    */
+
+    /**
+     * entonces la generación de un jwt es:
+     * base64(header).base64(payload).base64(signature)
+     * donde signature es:
+     * hmacsha[x]
+     * donde x = mezcla de ((a + b), c) donde c es algo como la "salt" del hasheo, pero constante 
+     * donde a = base64(header)
+     * b = base64(payload)
+     * c = secret
+     * 
+     * Entonces cuando toca validarlo, tomas el "b64(header).b64(payload)", lo metes al hmacsha(AQUI,secret) y lo b64amos, y si sale igual, todo cuadra. 
      */
-    const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword); //match será booleano
-    if(!match) throw new UnauthorizedException('No estás autorizado');
-    return "OK"; //si no lanza excepción. 
   }
 
 }
